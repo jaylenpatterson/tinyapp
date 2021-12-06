ba816f364341649777a726dd8f6d96db10cab420
jaylenpatterson <jaylenpatterson16@gmail.com>
2021-12-06 21:40:19 +0000

    added ejs style html

--- express_server.js
+++ express_server.js
@@ -33,3 +33,12 @@ app.get("/set", (req, res) => {
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  
+  res.render("urls_index", templateVars);
+  
+});
+
+
